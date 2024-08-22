b3822c2d9e7141be405a1a21835320bd4b2d9b6a
s3840980 <s3840980@student.rmit.edu.au>
2024-08-22 16:55:47 +1000

    files from assignment one folder
    I did not notice that there was an a1 folder intended for the assignment

---
+++ a1/js/main.js
@@ -0,0 +1,8 @@
+function navigateToPage(page) {
+    if (page) {
+        window.location.href = page;
+    }
+}
+document.getElementById('menu').addEventListener('change', function() {
+    navigateToPage(this.value);
+});
