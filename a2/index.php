0329f129cd00dab01fbba434321bba13e0cbafb9
s3840980 <s3840980@student.rmit.edu.au>
2024-10-03 11:45:32 +1000

    Update index.php

--- a2/index.php
+++ a2/index.php
@@ -1,18 +1,21 @@
 <?php
-include('includes/db_connect.inc');
+include('includes/db_connect.inc'); 
 include('includes/header.inc');
 include('includes/nav.inc');
 ?>
+
 <main>
-            
-            <h1 class="site-title">PETS VICTORIA</h1>
-            <h2 class="subheading">WELCOME TO PET 
-                <br> ADOPTION</h2>
-            
-        <img src="images/main.jpg" alt="A puppy and a kitten cuddling" class="index-image">
+    <h1 class="site-title">PETS VICTORIA</h1>
+    <h2 class="subheading">WELCOME TO PET 
+        <br> 
+        ADOPTION</h2>
+    
+    <img src="images/main.jpg" alt="A puppy and a kitten cuddling" class="index-image">
 </main>
+
 <?php
+// Include the footer
 include('includes/footer.inc');
 ?>
-    <script src="js/main.js"></script>
-    
+
+<script src="js/main.js"></script>
