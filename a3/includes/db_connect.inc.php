25a53190d0c61594795abc40ab0a627ba65ce980
s3840980 <s3840980@student.rmit.edu.au>
2024-11-07 18:59:44 +1100

    Create db_connect.inc.php

---
+++ a3/includes/db_connect.inc.php
@@ -0,0 +1,17 @@
+<?php
+// Database connection file (db_connect.inc)
+
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "petsvictoria"; 
+
+try {
+    
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
