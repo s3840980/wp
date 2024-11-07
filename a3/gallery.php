611a1565f0e9e9fa9a1452f83ff4af58ab65c9a5
s3840980 <s3840980@student.rmit.edu.au>
2024-11-08 01:44:01 +1100

    Update gallery.php

--- a3/gallery.php
+++ a3/gallery.php
@@ -6,7 +6,7 @@ include('includes/nav.inc');
 
 $petType = isset($_GET['pet_type']) ? $_GET['pet_type'] : '';
 
-$query = "SELECT name, image_path, id FROM pets WHERE 1=1"; 
+$query = "SELECT petname, image, petid FROM pets WHERE 1=1"; 
 
 if ($petType) {
     $query .= " AND type = ?";
@@ -14,7 +14,6 @@ if ($petType) {
 
 $stmt = $conn->prepare($query);
 
-// Check if the prepare statement was successful
 if ($stmt === false) {
     die("SQL preparation failed: " . htmlspecialchars($conn->error));
 }
@@ -34,9 +33,9 @@ $result = $stmt->get_result();
             <?php while ($pet = $result->fetch_assoc()): ?>
                 <div class="col-md-4">
                     <div class="pet-card">
-                        <a href="details.php?id=<?= $pet['id'] ?>" style="text-decoration: none;">
-                            <img src="<?= htmlspecialchars($pet['image_path']) ?>" alt="<?= htmlspecialchars($pet['name']) ?>" class="img-fluid">
-                            <div class="pet-name"><?= htmlspecialchars($pet['name']) ?></div>
+                        <a href="details.php?petid=<?= $pet['petid'] ?>" style="text-decoration: none;">
+                            <img src="<?= htmlspecialchars($pet['image']) ?>" alt="<?= htmlspecialchars($pet['petname']) ?>" class="img-fluid">
+                            <div class="pet-name"><?= htmlspecialchars($pet['petname']) ?></div>
                         </a>
                     </div>
                 </div>
