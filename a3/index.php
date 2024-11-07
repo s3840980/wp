5758207f89a94c4e4264e44593e22d0e4e4e1b0d
s3840980 <s3840980@student.rmit.edu.au>
2024-11-07 19:13:48 +1100

    Update index.php

--- a3/index.php
+++ a3/index.php
@@ -10,7 +10,7 @@ include('db_connect.inc.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pets Victoria - Home</title>
     <link rel="stylesheet" href="css/styles.css">
-    <!-- Bootstrap CSS CDN -->
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
