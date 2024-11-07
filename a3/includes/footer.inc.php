6904b52661ea8504d081040be505fa8e9d082069
s3840980 <s3840980@student.rmit.edu.au>
2024-11-07 19:59:55 +1100

    Create footer.inc.php

---
+++ a3/includes/footer.inc.php
@@ -0,0 +1,8 @@
+<footer class="text-center mt-4">
+    <p>&copy; <?php echo date("Y"); ?> Pets Victoria. All rights reserved.</p>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
