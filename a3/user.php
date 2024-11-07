ea4a7a56ac5b0d199482dfb8ab883cf56b012b82
s3840980 <s3840980@student.rmit.edu.au>
2024-11-07 23:02:05 +1100

    Update user.php

--- a3/user.php
+++ a3/user.php
@@ -1,9 +1,9 @@
 <?php
 session_start(); 
 $title = "User Pets Page";
-include('include/db_connect.inc'); 
-include('include/header.inc'); 
-include('include/nav.inc'); 
+include('includes/db_connect.inc'); 
+include('includes/header.inc'); 
+include('includes/nav.inc'); 
 
 
 if (!isset($_SESSION['id'])) {
@@ -57,4 +57,4 @@ $result = $stmt->get_result();
     ?>
 </main>
 
-<?php include('include/footer.inc'); ?>
+<?php include('includes/footer.inc'); ?>
